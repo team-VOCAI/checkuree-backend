ab943a2c0a0e0d7531020eceec9ee1dd397b25f6
ssini-oh <ssini8280@gmail.com>
2025-10-13 15:27:33 +0900

    Feat/auth (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { ChecklistModule } from './modules/checklist/checklist.module';
 
 @Module({
   imports: [
     AuthModule,
-    ChecklistModule,
-    // TODO: UserModule, CheckitemModule 추가 예정
+    // TODO: ChecklistModule, UserModule, CheckitemModule 추가 예정
   ],
   controllers: [AppController],
   providers: [AppService],
