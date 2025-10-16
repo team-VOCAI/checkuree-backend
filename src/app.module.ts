0af85876e34c6b5d90deb844915161e833e352b4
ssini-oh <ssini8280@gmail.com>
2025-10-17 01:32:11 +0900

    feat: 로그인, 리프레시 토큰 api 구현 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 환경 변수 사용 가능
+      envFilePath: '.env',
+    }),
     AuthModule,
     // TODO: ChecklistModule, UserModule, CheckitemModule 추가 예정
   ],
